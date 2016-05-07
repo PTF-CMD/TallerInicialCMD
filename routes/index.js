e330bcef965fb68ad87452c1998a55fd91cfc732
JuanIgnacioCuiule <juanignaciocuiule@gmail.com>
2016-05-07 12:00:08 -0400

    server local

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/calculadora_iva',function(req, res, next) {
+	res.render('calculadora_iva');
+})
+
+router.get('/lista_de_compras',function(req, res, next) {
+	res.render('lista_de_compras');
+})
+
+router.get('/piedra_papel_tijera',function(req, res, next) {
+	res.render('piedra_papel_tijera');
+})
+
 module.exports = router;
